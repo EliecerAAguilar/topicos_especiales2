cdfa5a025512eff9b5d0f6afb227473ef6128027
Eliecer <eliecer.aguilar1@utp.ac.pa>
2022-05-16 01:29:39 -0500

    adding module euclidean, printing values

--- pyFiles/Euclidean.py
+++ pyFiles/Euclidean.py
@@ -1,12 +1,11 @@
 from math import pow, sqrt
 
 
-class EuclideanD:
-    def __init__(self, x1, x2, y1, y2):
-        self.x1 = x1
-        self.x2 = x2
-        self.y1 = y1
-        self.y2 = y2
+def euclideanDistance(x2, x1, y2, y1):
+    return sqrt(pow((x2-x1), 2)+pow((y2-y1), 2))
 
-    def euclideanDistance(self, x1, y1, x2, y2):
-        return sqrt(pow((x2-x1), 2)+pow((y2-y1), 2))
+
+def euclideanDistance2(x2, x1, y2, y1):
+    return sqrt(pow((x2-x1), 2)+pow((y2-y1), 2))
+
+
