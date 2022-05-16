ff77967e0310019d8d575380a174915013845f07
Eliecer <eliecer.aguilar1@utp.ac.pa>
2022-05-16 03:33:14 -0500

    json values

--- pyFiles/Euclidean.py
+++ pyFiles/Euclidean.py
@@ -1,11 +1,11 @@
 from math import pow, sqrt
 
 
-def euclideanDistance(x2, x1, y2, y1):
+def euclideanDistance(x1, y1, x2, y2):
     return sqrt(pow((x2-x1), 2)+pow((y2-y1), 2))
 
 
-def euclideanDistance2(x2, x1, y2, y1):
+def euclideanDistance2(x1, y1, x2, y2):
     return sqrt(pow((x2-x1), 2)+pow((y2-y1), 2))
 
 
